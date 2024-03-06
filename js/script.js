8ecb2e0efe80e0b5117fbaa1eb1215738457d2e5
Mohammad Hisham <mohammadhesham65@gmail.com>
2024-03-06 15:17:34 +0200

    js modification

--- js/script.js
+++ js/script.js
@@ -1,48 +1,38 @@
-let links = document.querySelectorAll('.nav-links li');
 
-for (let i = 0; i < links.length; i++) {
-  links[i].addEventListener('click', function(e) {
-    for (let j = 0; j < links.length; j++) {
-      links[j].classList.remove('active');
-    }
+var links = document.querySelectorAll('.nav-links li');
+
+
+links.forEach(function(link) {
+  link.addEventListener('click', function(e) {
+    
+    links.forEach(function(link) {
+      link.classList.remove('active');
+    });
+   
     e.target.classList.add('active');
   });
-}
+});
 
-let headerHeight = $("nav").height();
 
- $('a[href*="#"]')
-  
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      var scrollToPosition = $(target).offset().top - headerHeight;
-     
-      if (target.length) {
-       
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: scrollToPosition
-        }, 750, function() {
-          
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { 
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); 
-            $target.focus(); 
-          };
-        });
-      }
-    }
+var headerHeight = document.querySelector("nav").offsetHeight;
+
+
+var anchorLinks = document.querySelectorAll('a[href*="#"]:not([href="#"]):not([href="#0"])');
+
+
+anchorLinks.forEach(function(link) {
+  link.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    var target = document.querySelector(this.hash);
+    target = target ? target : document.querySelector('[name=' + this.hash.slice(1) + ']');
+
+    var scrollToPosition = target.getBoundingClientRect().top + window.pageYOffset - headerHeight;
+
+    window.scrollTo({
+      top: scrollToPosition,
+      behavior: 'smooth'
+    });
+    target.focus();
   });
+});
